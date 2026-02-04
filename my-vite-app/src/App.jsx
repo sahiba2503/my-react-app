0736448ce4dc820829bbe9a068c4cc1f80ce4f9c
sahiba <25sahiba03@gmail.com>
2026-02-04 22:05:20 +0530

    add use hook

--- my-vite-app/src/App.jsx
+++ my-vite-app/src/App.jsx
@@ -3,8 +3,12 @@ import LeftSlidebar from "./LeftSidebar";
 import Navbar  from "./Navbar"; 
 import  RightSidebar from './RightSidebar';
 import Footer from "./Footer.jsx";
-
+import {useEffect} from 'react'
 function App() {
+  // only first time will load the useEffect fun when i use []
+  useEffect(()=>{
+    console.log("Hello!");
+  });
   return (
     <>
      <Navbar />
