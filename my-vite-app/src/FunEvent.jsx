a90baf8e3e261cd3363d1dafd52fb7e90d328eb0
sahiba <25sahiba03@gmail.com>
2026-03-04 21:15:25 +0530

    add function and event

---
+++ my-vite-app/src/FunEvent.jsx
@@ -0,0 +1,15 @@
+// rafce
+
+
+const FunEvent = () => {
+    function getName(){
+        return "Sahiba";
+    }
+  return (
+    <div>
+      <h1>Hello {getName()}</h1>
+    </div>
+  )
+}
+
+export default FunEvent;
--- my-vite-app/src/Styling.jsx
+++ my-vite-app/src/Styling.jsx
@@ -1,13 +1,33 @@
-import "./seperateSty.css";
-import styles from "./Styling.module.css";
-function Styling(){
 
+function Styling(){
+    const lastName = "parveen"
+    const fullName = "sahiba Parveen"
+function getName(){
+        return "Sahiba";
+    }
+    function getLastName(){
+        return lastName;
+    }
+    // function with parameter
+    //   function getFullName(fullName){
+    //     return fullName;
+    // }
+    // Arrow function with parameter
+     const getFullName=(fullName)=>{
+        return fullName;
+    }
+    function EventHandle(){
+        alert("button was clicked");
+    }
 
 return<>
 <div>
-<h1 style={{color:"red",textAlign:"center"}}>inline styling</h1>
-<h2 className="ExternalSty">Externat Styling</h2>
-<h3 className={styles.test}>css module</h3>
+    <h1 style={{color:"green",textAlign:"center"}}>Function and Event </h1>
+<h1 style={{textAlign:"center"}}>good morning</h1>
+<h2 style={{textAlign:"center",color:"red"}}>Hello {getName()}</h2>
+<h2 style={{textAlign:"center"}}>Hello {getLastName()}</h2>
+<h2 style={{textAlign:"center",color:"blue"}}>Hello {getFullName(fullName)}</h2>
+<button style={{marginLeft:"47%"}} onClick={EventHandle}> click me </button>
 </div>
 </>
 }
