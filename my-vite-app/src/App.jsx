c2e8c12854b94056eebcec765e68f23e9a7cc225
sahiba <25sahiba03@gmail.com>
2026-03-07 23:10:12 +0530

    add conditional rendering

--- my-vite-app/src/App.jsx
+++ my-vite-app/src/App.jsx
@@ -1,64 +1,41 @@
 
 
-// // import LeftSidebar from "./LeftSidebar";
-// // import Navbar from "./Navbar"; 
-// // import RightSidebar from "./RightSidebar";
-// // import Footer from "./Footer.jsx";
-// import Styling from "./Styling.jsx"
-//  import UseState from "./assets/UseState.jsx";
-//  import UseRef from "./UseRef.jsx";
-// import { useEffect } from "react";
-// import Useprop from "./Useprop";
-// import ConditionalRen from "./ConditionalRen.jsx";
-// function App() {
-  
-//    useEffect(()=>{
-//     console.log("componet loaded");
-//     alert("component is loaded")
-   
-//    },[]
-
-//    );
-  
-  
-//     return (
-//     <>
-//       {/* <Navbar />
-//       <LeftSidebar />
-//       <RightSidebar />
-//       <Footer /> */}
-      
-//       <Styling />
-//       <UseState />
-//        <UseRef />
-//        <Useprop />
-//        <ConditionalRen />
-//     </>
-//   );
-// }
-
-//  export default App;
-// //[] means run only once
-
-import Styling from "./Styling.jsx";
-import UseState from "./assets/UseState.jsx";
-import UseRef from "./UseRef.jsx";
-import Useprop from "./Useprop.jsx";
-import ConditionalRen from "./ConditionalRen.jsx";
+// import LeftSidebar from "./LeftSidebar";
+// import Navbar from "./Navbar"; 
+// import RightSidebar from "./RightSidebar";
+// import Footer from "./Footer.jsx";
+import Styling from "./Styling.jsx"
+ import UseState from "./assets/UseState.jsx";
+ import UseRef from "./UseRef.jsx";
+import { useEffect } from "react";
+import Useprop from "./Useprop";
+import ConditionalRen from "../ConditionalRen.jsx";
 function App() {
+  
+   useEffect(()=>{
+    console.log("componet loaded");
+    alert("component is loaded")
+   
+   },[]
 
- 
-
-  return (
+   );
+  
+  
+    return (
     <>
+      {/* <Navbar />
+      <LeftSidebar />
+      <RightSidebar />
+      <Footer /> */}
+      
       <Styling />
       <UseState />
-      <UseRef />
+       <UseRef />
+       <Useprop />
        <ConditionalRen />
-      <Useprop />
-     
     </>
   );
 }
 
-export default App;
+ export default App;
+//[] means run only once
